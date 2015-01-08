db2ab441c24d2b62275d9d3e6e00cc33a80c061d
郭明锋 <mf.guo@qq.com>
2015-01-08 14:43:23 +0800

    添加并逐步完善OSharp示例项目，所有OSharp框架的类库都由nuget引用

---
+++ samples/OSharp.Demo.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using OSharp.Demo.Web.Models;
+using OSharp.Demo.Web.Services;
+using OSharp.Utility.Extensions;
+
+
+namespace OSharp.Demo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IIdentityContract _identityContract;
+
+        public HomeController(IIdentityContract identityContract)
+        {
+            _identityContract = identityContract;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Data = new
+            {
+                OrganizationCount = _identityContract.Organizations.Count(),
+                UserCount = _identityContract.Users.Count(),
+                RoleCount = _identityContract.Roles.Count()
+            }.ToDynamic();
+            return View();
+        }
+	}
+}
---
+++ samples/OSharp.Demo.Web/Global.asax.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Mapping;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+using Autofac;
+using Autofac.Integration.Mvc;
+
+using OSharp.Core;
+using OSharp.Core.Data;
+using OSharp.Core.Data.Entity;
+using OSharp.Demo.Web.Dtos;
+
+
+namespace OSharp.Demo.Web
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            RoutesRegister();
+            DtoMappers.MapperRegister();
+            AutofacMvcRegister();
+            DatabaseInitialize();
+        }
+
+        private static void RoutesRegister()
+        {
+            RouteCollection routes = RouteTable.Routes;
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "OSharp.Demo.Web.Controllers" });
+        }
+
+        private static void AutofacMvcRegister()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterGeneric(typeof(Repository<,>)).As(typeof(IRepository<,>));
+            Type baseType = typeof(IDependency);
+            Assembly[] assemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies()
+                .Select(Assembly.Load).ToArray();
+            assemblies = assemblies.Union(new[] { Assembly.GetExecutingAssembly() }).ToArray();
+            builder.RegisterAssemblyTypes(assemblies)
+                .Where(type => baseType.IsAssignableFrom(type) && !type.IsAbstract)
+                .AsImplementedInterfaces().InstancePerLifetimeScope();//InstancePerLifetimeScope 保证生命周期基于请求
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterFilterProvider();
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private static void DatabaseInitialize()
+        {
+            DatabaseInitializer.Initialize();
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            DatabaseInitializer.AddMapperAssembly(assembly);
+
+            //EF预热
+            using (CodeFirstDbContext context = new CodeFirstDbContext())
+            {
+                ObjectContext objectContext = ((IObjectContextAdapter)context).ObjectContext;
+                StorageMappingItemCollection mappingItemCollection = (StorageMappingItemCollection)objectContext.ObjectStateManager
+                    .MetadataWorkspace.GetItemCollection(DataSpace.CSSpace);
+                mappingItemCollection.GenerateViews(new List<EdmSchemaError>());
+            }
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
