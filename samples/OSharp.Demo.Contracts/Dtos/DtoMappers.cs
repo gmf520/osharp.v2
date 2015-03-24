c0f28f16a851080b4c4e0343c24c29bae0df3989
gmf520 <gmf520@yeah.net>
2015-03-24 22:07:14 +0800

    补签入上次未成功提交的文件

---
+++ samples/OSharp.Demo.Contracts/Dtos/DtoMappers.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="DtoMappers.cs" company="OSharp开源团队">
+//      Copyright (c) 2014-2015 OSharp. All rights reserved.
+//  </copyright>
+//  <last-editor>郭明锋</last-editor>
+//  <last-date>2015-03-24 16:13</last-date>
+// -----------------------------------------------------------------------
+
+using AutoMapper;
+
+using OSharp.Demo.Dtos.Identity;
+using OSharp.Demo.Models.Identity;
+
+
+namespace OSharp.Demo.Dtos
+{
+    public class DtoMappers
+    {
+        public static void MapperRegister()
+        {
+            //Identity
+            Mapper.CreateMap<OrganizationDto, Organization>();
+            Mapper.CreateMap<UserDto, User>();
+            Mapper.CreateMap<RoleDto, Role>();
+        }
+    }
+}
---
+++ samples/OSharp.Demo.Services/ModelConfigurations/Identity/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="UserConfiguration.cs" company="OSharp开源团队">
+//      Copyright (c) 2014-2015 OSharp. All rights reserved.
+//  </copyright>
+//  <last-editor>郭明锋</last-editor>
+//  <last-date>2015-03-24 17:04</last-date>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OSharp.Demo.ModelConfigurations.Identity
+{
+    public partial class UserConfiguration
+    {
+        partial void UserConfigurationAppend()
+        {
+            HasMany(m => m.Roles).WithMany(n => n.Users);
+        }
+    }
+}
